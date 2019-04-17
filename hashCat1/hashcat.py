c0ff2994514cef95a00d1fb2f0d77583a58f833f
Santosh Gokul <santoanand2@gmail.com>
2019-04-18 00:36:34 +0530

    Add files via upload
    I've committed hashcat based program.

---
+++ hashCat1/hashcat.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+print("This system makes use of hashcat as it's password attack tool!!")
+print("\nMake sure that all your targeted password hashes are in hash.txt......")
+
+os.system("hashcat -a 0 -m 1700 hash.txt rockyou.splitaa rockyou.splitab rockyou.splitac rockyou.splitad rockyou.splitae rockyou.splitaf rockyou.splitag -r rule64.txt -o cracked.txt")
+
+
+print("\nThe Cracked Passwords are:\n\n")
+with open("hash.txt") as file1:
+	with open("cracked.txt") as file2:	
+		for line1 in file1:
+			for line2 in file2:
+				op = re.search(line1.rstrip(),line2.rstrip(),re.IGNORECASE)     
+				if(op!=None):
+					print(line2)
+
